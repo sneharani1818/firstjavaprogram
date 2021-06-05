20401ba3cee6ca1f4ad3ecd88c4d4a3ca6170939
sneharani1818 <85399970+sneharani1818@users.noreply.github.com>
2021-06-05 19:03:18 +0530

    Created JavaApplication1.java
    This program prints hello world on the screen.

---
+++ JavaApplication1.java
@@ -0,0 +1,9 @@
+//This is my first java program that prints 'hello world'
+package javaapplication1;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world!!");
+    }
+    
+}
